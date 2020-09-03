fc3c778d19f491311cd4d6a14e63b171871d7103
rekvi <rekviemi@mail.ru>
2020-09-03 21:58:53 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -3,8 +3,6 @@ package ru.progwards.java1.lessons.basics;
 public class ReverseDigits {
 
     public static int reverseDigits(int number){
-        System.out.println(number % 10);
-        return (number/10 > 0) ? reverseDigits(number/10) : number;
+        return number % 10;
     }
-
 }

07e4e739c7023fdff89d2a0d16ab5c73b4736c94
rekvi <rekviemi@mail.ru>
2020-09-05 21:02:29 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -1,15 +1,18 @@
 package ru.progwards.java1.lessons.basics;
 
-public class ReverseDigits {
+class ReverseDigits {
+    public static void main(String[] args){}
 
-    public static void main(String[] args) {
 
-        int a = 123 / 100;
-        int b = 123 / 10 % 10;
-        int c = 123 % 10;
 
-        System.out.print(c);
-        System.out.print(b);
-        System.out.print(a);
+        public static int reverseDigits( int number){
+            int a = number / 100;
+            int b = number / 10 % 10;
+            int c = number % 10;
+
+            System.out.print(c);
+            System.out.print(b);
+            System.out.print(a);
+            return c + b + a;
+        }
     }
-}
--- src/ru/rekvi/sas/Test.java
+++ src/ru/rekvi/sas/Test.java
@@ -3,15 +3,18 @@ package ru.rekvi.sas;
 class Test {
     public static void main(String[] args) {
 
-        int a = 123 / 100;
-        int b = 123 / 10 % 10;
-        int c = 123 % 10;
+
+    }
+
+    public static int reverseDigits(int number) {
+        int a = number / 100;
+        int b = number / 10 % 10;
+        int c = number % 10;
 
         System.out.print(c);
         System.out.print(b);
         System.out.print(a);
-
-
+        return a + b + c;
     }
 
 }

0b5ae888c5414f7f6f410afb03d16a737f14fd39
rekvi <rekviemi@mail.ru>
2020-09-09 21:09:56 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -1,46 +1,54 @@
 package ru.progwards.java1.lessons.basics;
 
-public class Astronomy {
-    public static void main(String[] args) {
-        double s1 = earthSquare();
-        System.out.println( + s1);
 
-        double s2 = mercurySquare();
-        System.out.println( + s2);
+    public class Astronomy{
+        public static void main(String[] args) {
+            double s1 = earthSquare();
+            System.out.println( + s1);
 
-        double s3 = jupiterSquare();
-        System.out.println( + s3);
+            double s2 = mercurySquare();
+            System.out.println( + s2);
+
+            double s3 = jupiterSquare();
+            System.out.println( + s3);
+
+            double s4 = earthVsMercury();
+            System.out.println( + s4);
+
+            double s5 = earthVsJupiter();
+            System.out.println( + s5);
+        }
+
+        public static Double sphereSquare(Double r){
+            double s = (4 * 3.14) * (r * r);
+            return s;
+        }
+
+        public static Double earthSquare(){
+            double s1 = sphereSquare(6371.2);
+            return s1;
+        }
+
+        public static Double mercurySquare(){
+            double s2 = sphereSquare(2439.7);
+            return s2;
+        }
+
+        public static Double jupiterSquare(){
+            double s3 = sphereSquare(71492.00);
+            return s3;
+        }
+        public static Double earthVsMercury(){
+            double s1 = sphereSquare(6371.2);
+            double s2 = sphereSquare(2439.7);
+            double S = s1 / s2;
+            return S;
+        }
+
+        public static Double earthVsJupiter(){
+            double s1 = sphereSquare(6371.2);
+            double s2 = sphereSquare(71492.00);
+            double S = s1 / s2;
+            return S;
+        }
     }
-
-    public static Double sphereSquare(Double r){
-        double s = (4 * 3.14) * (r * r);
-        return s;
-    }
-
-    public static Double earthSquare(){
-        double r = 6371.2;
-        double s1 = (4 * 3.14) * (r * r);
-        return s1;
-    }
-
-   public static Double mercurySquare(){
-       double r = 2439.7;
-       double s2 = (4 * 3.14) * (r * r);
-       return s2;
-   }
-
-   public static Double jupiterSquare(){
-       double r = 71492.00;
-       double s3 = (4 * 3.14) * (r * r);
-       return s3;
-   }
-    public static Double earthVsMercury(){
-        double r = 1;
-        return r;
-    }
-
-    public static Double earthVsJupiter(){
-        double r = 1;
-        return r;
-    }
-}
--- src/ru/rekvi/sas/Test.java
+++ src/ru/rekvi/sas/Test.java
@@ -1,15 +1,56 @@
 package ru.rekvi.sas;
 
+
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(sumDigitsInNumber(123));
+        double s1 = earthSquare();
+        System.out.println( + s1);
+
+        double s2 = mercurySquare();
+        System.out.println( + s2);
+
+        double s3 = jupiterSquare();
+        System.out.println( + s3);
+
+        double s4 = earthVsMercury();
+        System.out.println( + s4);
+
+        double s5 = earthVsJupiter();
+        System.out.println( + s5);
     }
 
-    public static int sumDigitsInNumber(int number) {
-        int a = number / 100;
-        int b = number / 10 % 10;
-        int c = number % 10;
-        return (c * 100) + (b * 10) + a;
+    public static Double sphereSquare(Double r){
+        double s = (4 * 3.14) * (r * r);
+        return s;
+    }
+
+    public static Double earthSquare(){
+        double s1 = sphereSquare(6371.2);
+        return s1;
+    }
+
+    public static Double mercurySquare(){
+        double s2 = sphereSquare(2439.7);
+        return s2;
+    }
+
+    public static Double jupiterSquare(){
+        double s3 = sphereSquare(71492.00);
+        return s3;
+    }
+    public static Double earthVsMercury(){
+        double s1 = sphereSquare(6371.2);
+        double s2 = sphereSquare(2439.7);
+        double S = s1 / s2;
+        return S;
+    }
+
+    public static Double earthVsJupiter(){
+        double s1 = sphereSquare(6371.2);
+        double s2 = sphereSquare(71492.00);
+        double S = s1 / s2;
+        return S;
     }
 }
 

cca3cd1120db061cd4fb3de1b6d693ffb551801f
rekvi <rekviemi@mail.ru>
2020-09-11 19:45:16 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -8,20 +8,20 @@ public class AccuracyDoubleFloat {
     }
 
     public static double volumeBallDouble(double radius){
-    double r = 4 / (3 * 3.14) * (6371.1 * 6371.1 * 6371.1);
-    return r;
+    double a = 4.00 / 3 * 3.14 * (6371.1 * 6371.1 * 6371.1);
+    return a;
     }
 
     public static float volumeBallFloat(float radius){
-    float t =  4F / (3F * 3.14F) * (6371.1F * 6371.1F * 6371.1F);
+    float t =  4F / 3F * 3.14F * (6371.1F * 6371.1F * 6371.1F);
     return t;
     }
 
-    public static double calculateAccuracy(double radius){
-        double d = (float) radius;
-        double s = volumeBallDouble(6371.1);
-        float s1 = volumeBallFloat(6371.1F);
-        double s2 = s / s1;
-        return s2;
-    }
+   // public static double calculateAccuracy(double radius){
+     //   double d = (float) radius;
+    //    double s = volumeBallDouble(6371.1);
+    //    float s1 = volumeBallFloat(6371.1F);
+    //    double s2 = s / s1;
+     //   return s2;
+   // }
 }

196a74b8b51af987e85aee963ca9a430d9f5d327
rekvi <rekviemi@mail.ru>
2020-09-21 20:11:04 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -2,43 +2,48 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static void main(String[] args) {
-        System.out.println(maxSide(1, 2, 3));
-        System.out.println(minSide(1, 2, 3));
+        System.out.println(maxSide(8, 7, 8));
+        System.out.println(minSide(8, 8, 7));
         System.out.println(isEquilateralTriangle(1, 2, 3));
     }
 
     public static int maxSide(int a, int b, int c) {
-        int side;
-        if (a > b && a > c) {
-            side = a;
-        } else if (c < b && c < a) {
-            side = c;
-        } else if (b > a && b > c) {
-            side = b;
-        } else if (c > b && c > a) {
-            side = c;
-        } else {
-            side = 0;
-        }
-        return side;
+       int max;
+//        if (a > b && a > c) {
+//            max = a;
+//        } else if (c < b && c < a) {
+//            max = c;
+//        } else if (b > a && b > c) {
+//            max = b;
+//        } else if (c > b && c > a) {
+//            max = c;
+//        } else {
+//            max = 0;
+//        }
+//        return side;
+        max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        return max;
     }
 
-    public static int minSide(int a, int b, int c){
+    public static int minSide(int a, int b, int c) {
 
-        int side;
-        if (a < b && a < c) {
-            side = a;
-        } else if (c > b && c > a) {
-            side = c;
-        } else if (b < a && b < c) {
-            side = b;
-        } else if (c < b && c < a) {
-            side = c;
-        } else {
-            side = 0;
-        }
-        return side;
-        }
+        int min;
+//        if (a < b && a < c) {
+//            min = a;
+//        } else if (c > b && c > a) {
+//            min = c;
+//        } else if (b < a && b < c) {
+//            min = b;
+//        } else if (c < b && c < a) {
+//            side = c;
+//        } else {
+//            min = 0;
+//        }
+//        return side;
+//        }
+        min = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+        return min;
+    }
     public static boolean isEquilateralTriangle(int a, int b, int c){
 boolean result = a == b && b == c;
 return result;

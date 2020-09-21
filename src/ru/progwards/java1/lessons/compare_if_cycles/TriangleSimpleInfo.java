9a8e2af82c120b4e34a0b5ff5a9cda302e5c80ed
rekvi <rekviemi@mail.ru>
2020-09-21 19:33:19 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -2,9 +2,9 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static void main(String[] args) {
-        System.out.println(maxSide(10, 20, 30));
-        System.out.println(minSide(10, 20, 30));
-        System.out.println(isEquilateralTriangle(10, 10, 10));
+        System.out.println(maxSide(1, 2, 3));
+        System.out.println(minSide(1, 2, 3));
+        System.out.println(isEquilateralTriangle(1, 2, 3));
     }
 
     public static int maxSide(int a, int b, int c) {
@@ -24,20 +24,21 @@ public class TriangleSimpleInfo {
     }
 
     public static int minSide(int a, int b, int c){
+
         int side;
         if (a < b && a < c) {
             side = a;
-        } else if (c < b && c < a) {
-            side = c;
-        } else if (b > a && b > c) {
-            side = b;
         } else if (c > b && c > a) {
+            side = c;
+        } else if (b < a && b < c) {
+            side = b;
+        } else if (c < b && c < a) {
             side = c;
         } else {
             side = 0;
         }
         return side;
-    }
+        }
     public static boolean isEquilateralTriangle(int a, int b, int c){
 boolean result = a == b && b == c;
 return result;

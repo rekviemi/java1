df79dc9ed9e98e1ee0050fa525d5d1bf0c58ea22
rekvi <rekviemi@mail.ru>
2020-09-22 20:51:03 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -2,8 +2,8 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
     public static void main(String[] args) {
-        System.out.println(isTriangle(4, 1, 5));
-        System.out.println(isRightTriangle(4, 4, 8));
+        //System.out.println(isTriangle(4, 1, 5));
+        System.out.println(isRightTriangle(5, 3, 4));
     }
     public static boolean isTriangle(int a, int b, int c){
 
@@ -11,7 +11,7 @@ public class TriangleInfo {
      return max;
         }
     public static boolean isRightTriangle(int a, int b, int c){
-        boolean max = ((a*a) + (b*b) == (c*c)) && ((a*a) + (c*c) == (b*b)) && ((b*b) + (c*c) == (a*a));
+        boolean max = (a*a) + (b*b) == (c*c) || ((a*a) + (c*c) == (b*b)) || ((b*b) + (c*c) == (a*a));
         return max;
     }
     public static boolean isIsoscelesTriangle(int a, int b, int c){

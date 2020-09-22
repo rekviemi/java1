dcaabd93766c43e628d1fc1e7d165aec2deb1aa7
rekvi <rekviemi@mail.ru>
2020-09-22 19:41:19 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -1,4 +1,20 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
+    public static void main(String[] args) {
+        System.out.println(isTriangle(2, 4, 8));
+    }
+    public static boolean isTriangle(int a, int b, int c){
+
+        boolean max = (a + b > c) || (a + c > b) & (b + c > a);
+     return max;
+        }
+    public static boolean isRightTriangle(int a, int b, int c){
+        boolean max = (a + b > c) || (a + c > b) & (b + c > a);
+        return max;
+    }
+    public static boolean isIsoscelesTriangle(int a, int b, int c){
+        boolean max = (a + b > c) || (a + c > b) & (b + c > a);
+        return max;
+    }
 }

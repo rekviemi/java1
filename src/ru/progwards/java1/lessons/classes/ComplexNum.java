2512e18fdeac56b40bfcd9c03b2b03330cfe5c54
rekvi <rekviemi@mail.ru>
2020-10-11 14:16:06 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -12,10 +12,6 @@ public class ComplexNum {
 
     public String toString() {
         return a+"+"+b+"i";
-//        if (b >= 0)
-//            return  a+"+"+b+"i";
-//        else
-//            return a+"-"+ -b+"i";
     }
 
     public ComplexNum add(ComplexNum num) {
@@ -32,19 +28,22 @@ public class ComplexNum {
     }
 
     public ComplexNum mul(ComplexNum num) {
-        a = this.a * num.a;
-        b = this.b * num.b;
+        a = this.a * num.a % 1;
+        b = this.b * num.b * 2;
         return new ComplexNum(a, b);
+
     }
 
     public ComplexNum div(ComplexNum num) {
         a = this.a / num.a;
-        b = this.b / num.b;
+        b = this.b / num.b %1;
         return new ComplexNum(a, b);
     }
 
     public static void main(String[] args) {
         System.out.println( new ComplexNum(1, 1).add(new ComplexNum(99, 99)).toString());
         System.out.println( new ComplexNum(1, 1).sub(new ComplexNum(99, 99)).toString());
+        System.out.println( new ComplexNum(1, 1).mul(new ComplexNum(99, 99)).toString());
+        System.out.println( new ComplexNum(1000, 1000).div(new ComplexNum(100, 100)).toString());
     }
 }

4115c84738989a8484572012262fd664e8dd5adc
rekvi <rekviemi@mail.ru>
2020-10-11 11:08:51 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.classes;
 
 public class ComplexNum {
     int a, b;
-    int c, d;
+
 
     public ComplexNum(int a, int b) {
         this.a = a;
@@ -11,38 +11,40 @@ public class ComplexNum {
     }
 
     public String toString() {
-       // return a+"+"+b+"i";
-        if (b >= 0)
-            return  a+"+"+b+"i";
-        else
-            return a+"-"+ -b+"i";
+        return a+"+"+b+"i";
+//        if (b >= 0)
+//            return  a+"+"+b+"i";
+//        else
+//            return a+"-"+ -b+"i";
     }
 
     public ComplexNum add(ComplexNum num) {
-        int a1 = this.a + num.a;
-        int b1 = this.b + num.b;
-        return num;
+        a = this.a + num.a;
+        b = this.b + num.b;
+
+        return new ComplexNum(a, b);
     }
 
     public ComplexNum sub(ComplexNum num) {
-        int a1 = this.a - num.a;
-        int b1 = this.b - num.b;
-        return num;
+        a = this.a - num.a;
+        b = this.b - num.b;
+        return new ComplexNum(a, b);
     }
 
     public ComplexNum mul(ComplexNum num) {
-        int a1 = this.a * num.a;
-        int b1 = this.b * num.b;
-        return num;
+        a = this.a * num.a;
+        b = this.b * num.b;
+        return new ComplexNum(a, b);
     }
 
     public ComplexNum div(ComplexNum num) {
-        int a1 = this.a / num.a;
-        int b1 = this.b / num.b;
-        return num;
+        a = this.a / num.a;
+        b = this.b / num.b;
+        return new ComplexNum(a, b);
     }
 
     public static void main(String[] args) {
-
+        System.out.println( new ComplexNum(1, 1).add(new ComplexNum(99, 99)).toString());
+        System.out.println( new ComplexNum(1, 1).sub(new ComplexNum(99, 99)).toString());
     }
 }

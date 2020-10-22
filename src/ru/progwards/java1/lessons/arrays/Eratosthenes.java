def8fd45dd0461bd36df5e33f162981ffe9b82db
rekvi <rekviemi@mail.ru>
2020-10-22 10:23:27 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -3,31 +3,37 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class Eratosthenes {
+    private int N;
     private boolean[] sieve;
 
     public Eratosthenes(int N) {
         boolean[] sieve = new boolean[N];
         Arrays.fill(sieve, true);
-        sift();
     }
 
     private void sift() {
-
-        for (int i = 2; i * i < 0; i++) {
+        for (int i = 2; i <= N - 1; i++) {
             if (sieve[i]) {
-                for (int j = 2 * i; j < 0; j += i) {
+                for (int j = 2 * i; j <= N; j += i) {
                     sieve[j] = false;
                 }
             }
         }
     }
 
+
     public static boolean isSimple(int n) {
         if (n < 2) {
             return false;
         }
-        for (int i = 2; i * i > n; i++) {
-            if (n % i == 0) {
+        if (n % 2 == 0) {
+            return n == 2;
+        }
+        if (n % 3 == 0) {
+            return n == 3;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
         }
@@ -36,7 +42,7 @@ public class Eratosthenes {
 
     public static void main(String[] args) {
 
-        System.out.println(isSimple(54));
+        System.out.println(isSimple(2));
     }
 }
 

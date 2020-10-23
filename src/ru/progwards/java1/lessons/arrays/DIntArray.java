6e55984f68448c5c8d7959d9d7f5920fa27cd89b
rekvi <rekviemi@mail.ru>
2020-10-23 13:31:12 +0300

    HelloWorld

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -9,38 +9,52 @@ import java.util.Arrays;
 //public void add(int num) - добавляет элемент num в конец массива, при этом размер массива должен увеличиться на 1.
 // Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый, и добавить в хвост элемент num.
 //        3.3 метод
-//public void atInsert(int pos, int num) - добавляет элемент num в позицию pos массива, при этом размер массива должен увеличиться на 1. Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый, c учетом того, что новый элемент окажется где-то в середине, и потом положить в нужный индекс элемент num.
+//public void atInsert(int pos, int num) - добавляет элемент num в позицию pos массива, при этом размер
+// массива должен увеличиться на 1. Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый,
+// c учетом того, что новый элемент окажется где-то в середине, и потом положить в нужный индекс элемент num.
 //        3.4 метод
 //public void atDelete(int pos) - удаляет элемент в позиции pos массива, при этом размер массива должен уменьшиться на 1. Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый, уже без элемента, который был в позиции pos.
 //        3.5 метод
 //public int at(int pos) - возвращает элемент по индексу pos.
 public class DIntArray {
-    private int[] arrayInt;
+    private int[] arrayInt = {};
 
     public DIntArray() {
-        arrayInt = new int[0];
+        // arrayInt = new int[0];
     }
 
     public void add(int num) {
-        int[] arrayIntCopy = Arrays.copyOf(arrayInt, arrayInt.length + 1);
-        arrayIntCopy[arrayInt.length] = num;
-        arrayInt = Arrays.copyOf(arrayIntCopy, arrayIntCopy.length);
-        System.out.println(Arrays.toString(arrayInt));
+        int[] copyArrayInt = new int[arrayInt.length + 1];
+        System.arraycopy(arrayInt, 0, copyArrayInt, 0, arrayInt.length);
+        copyArrayInt[arrayInt.length] = num;
+        arrayInt = copyArrayInt;
     }
 
     public void atInsert(int pos, int num) {
-        return;
+        int[] copyArrayInt = new int[arrayInt.length + 1];
+        System.arraycopy(arrayInt, 0, copyArrayInt, 0, pos);
+        System.arraycopy(arrayInt, pos, copyArrayInt, pos + 1, arrayInt.length - pos);
+        copyArrayInt[pos] = num;
+        arrayInt = copyArrayInt;
     }
 
     public void atDelete(int pos) {
-        return;
+        int[] copyArrayInt = new int[arrayInt.length - 1];
     }
-    public int at(int pos){
-        return 0;
+
+    public int at(int pos) {
+        return arrayInt[pos];
+
     }
 
     public static void main(String[] args) {
-        DIntArray arrayInt = new DIntArray();
-        arrayInt.add(2);
+
+//            DIntArray arr = new DIntArray();
+//            arr.add(1);
+//            arr.add(2);
+//            arr.atInsert(5, 15);
+//            System.out.println(Arrays.toString(arr.arrayInt));
+
     }
 }
+
